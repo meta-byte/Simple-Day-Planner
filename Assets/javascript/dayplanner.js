619daba59c5d2142908da1e233c9f15800995500
Garrett Howard <hylander.garrett@gmail.com>
2019-12-18 18:57:46 -0700

    boxes now update with correct color scheme when time has been reached.

--- Assets/javascript/dayplanner.js
+++ Assets/javascript/dayplanner.js
@@ -1,9 +1,6 @@
 $(document).ready(function () {
 
-    var hoursArr = ["8:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00",]
-
-
-    // Get Current Time and Updates Page
+    // Gets current time to display on web page
     var currentTime = function () {
         timeEL = document.querySelector('#currentDay')
         timeEL.innerHTML = moment().format('MMMM Do YYYY, H:mm:ss');
@@ -11,14 +8,36 @@ $(document).ready(function () {
     }
     setInterval(currentTime, 1000);
 
-    function plannerRows() {
-        for (i = 0; i < hoursArr.length; i++) {
-            hourRow = $("<div>")
-            hourRow.attr("class", "timeRow future")
-            $("#agenda-box").append(hourRow)
-        }
+    //Updates rows to appropriate colors when time is reached
+    function hoursUpdater() {
+
+        var currentHour = moment().hours();
+        $('.time-block').each(function () {
+
+            var blockHour = parseInt($(this).attr("data-hour"))
+            // var blockHour = 13
+
+            console.log(blockHour);
+            console.log(currentHour);
+
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+                console.log("Past");
+            }
+            else if (blockHour > currentHour) {
+                $(this).addClass("future");
+                console.log("Future");
+
+            }
+            else {
+                $(this).addClass("present");
+                console.log("Present");
+            }
+
+            console.log(this)
+            setInterval(hoursUpdater, 60000)
+        })
     }
 
-    plannerRows();
+    hoursUpdater()
 })
-
