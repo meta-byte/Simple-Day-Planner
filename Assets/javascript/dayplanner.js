d46f6647f0f91fe8fb20869ac960c791fe92e49e
Garrett Howard <hylander.garrett@gmail.com>
2019-12-19 18:37:46 -0700

    tasks inputed are saved into an array as an object with their corresponding hour

--- Assets/javascript/dayplanner.js
+++ Assets/javascript/dayplanner.js
@@ -1,22 +1,20 @@
 $(document).ready(function () {
 
+    // var agenda = []
+    // var blockHour = ""
+
     // Gets current time to display on web page
     var currentTime = function () {
         timeEL = document.querySelector('#currentDay')
         timeEL.innerHTML = moment().format('MMMM Do YYYY, H:mm:ss');
-        // console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
     }
     setInterval(currentTime, 1000);
 
     //Updates rows to appropriate colors when time is reached
-    function hoursUpdater() {
-
+    var hoursUpdater = function () {
         var currentHour = moment().hours();
         $('.time-block').each(function () {
-
             var blockHour = parseInt($(this).attr("data-hour"))
-            // var blockHour = 13
-
             console.log(blockHour);
             console.log(currentHour);
 
@@ -33,11 +31,35 @@ $(document).ready(function () {
                 $(this).addClass("present");
                 console.log("Present");
             }
-
             console.log(this)
-            setInterval(hoursUpdater, 60000)
+            // setInterval(hoursUpdater, 60000)
+
         })
     }
+    // setInterval(hoursUpdater, 10000)
+
+    function saveAgenda() {
+        var agenda = []
+        $('.description').each(function () {
+            var hour = $(this).attr("data-task")
+            var task = $.trim($(this).val())
+            if (task != '') {
+                // agenda = JSON.parse(localStorage.getItem('item:'))
+
+                agenda.push({
+                    hour: hour,
+                    task: task,
+                })
+
+                localStorage.setItem('agendas:', JSON.stringify(agenda))
+
+            }
+        })
+
+    }
+
+
+    $(".saveBtn").click(saveAgenda)
 
     hoursUpdater()
 })
